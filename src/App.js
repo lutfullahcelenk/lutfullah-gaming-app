f6271c921a883c5607016f5ad1a7fccd09e73324
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-22 21:31:50 +0300

    Pagination component is added.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React,{useState} from 'react'
-import Header from './components/Header'
-import MainPage from './components/MainPage'
-import InitialState from "./store/InitialState"
-import {gameContext} from "./context/gameContext"
+import React,{useState} from 'react';
+import Header from './components/Header';
+import MainPage from './components/MainPage';
+import InitialState from "./store/InitialState";
+import {gameContext} from "./context/gameContext";
+import Pagination from './components/Pagination';
 
 
 const App = () => {
-  const[games,setGames] = useState(InitialState)
 
+  const[games,setGames] = useState(InitialState);
+  const [currentPage,setCurrentPage] = useState(1);
+  const[postsPerPage,setPostsPerPage] = useState(6);
+
+  //Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = games.slice(indexOfFirstPost,indexOfLastPost);
+
+  // Change Page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
-    <gameContext.Provider value={games}>
-    <div>
+    <gameContext.Provider value={currentPosts}>
+    
       <Header/>
       <MainPage/>
-    </div>
+      <Pagination postsPerPage={postsPerPage} totalPosts={games.length} paginate={paginate}/>
+    
     </gameContext.Provider>
   )
 }
-
 export default App
