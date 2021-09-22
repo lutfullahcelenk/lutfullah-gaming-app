bf2f01ecd99e9d0bce501df2275c76df8333deb5
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-22 21:33:08 +0300

    currentposts is pulled wtih context

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,7 +4,9 @@ import { useContext } from "react";
 import { gameContext } from "../context/gameContext";
 
 const MainPage = () => {
-  const games = useContext(gameContext);
+
+  const currentPosts = useContext(gameContext);
+  // console.log(currentPosts)
 
   return (
     <div>
@@ -15,9 +17,29 @@ const MainPage = () => {
         <button className="btn btn-primary btn-lg">
           <i className="fas fa-plus me-2"></i>Add Nominee
         </button>
+        <div class="dropdown">
+          <button
+            class="btn btn-secondary btn-lg"
+            type="button"
+            id="dropdownMenuButton"
+            data-bs-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <i className="fas fa-sort me-2"></i>Sort By
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <button class="dropdown-item btn h5" href="#">
+              Most Points
+            </button>
+            <button class="dropdown-item btn h5" href="#">
+              Less Points
+            </button>
+          </div>
+        </div>
       </div>
       <div className="main-container row w-75 m-auto">
-        {games.map((game) => (
+        {currentPosts.map((game) => (
           <Card key={game.id} game={game} />
         ))}
       </div>
