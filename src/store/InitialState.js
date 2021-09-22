f763e40546d8917645b91cd4db5be3d233c14132
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-22 15:24:43 +0300

    initialStates are written and sent to all components via context

---
+++ src/store/InitialState.js
@@ -0,0 +1,66 @@
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDay();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const myDate = `${year}/${month}/${day}  ${hours}:${minutes}`;
+
+const InitialState = [
+  {
+    id: 1,
+    point: 10,
+    name: "Cs Go",
+    winner: "Hakan",
+    votedate: myDate,
+    image:
+      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+  },
+  {
+    id: 2,
+    point: 10,
+    name: "Cs Go",
+    winner: "Hakan",
+    votedate: myDate,
+    image:
+      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+  },
+  {
+    id: 3,
+    point: 10,
+    name: "Cs Go",
+    winner: "Hakan",
+    votedate: myDate,
+    image:
+      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+  },
+  {
+    id: 4,
+    point: 10,
+    name: "Cs Go",
+    winner: "Hakan",
+    votedate: myDate,
+    image:
+      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+  },
+  {
+    id: 5,
+    point: 10,
+    name: "Cs Go",
+    winner: "Hakan",
+    votedate: myDate,
+    image:
+      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+  },
+  {
+    id: 6,
+    point: 10,
+    name: "Cs Go",
+    winner: "Hakan",
+    votedate: myDate,
+    image:
+      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+  },
+];
+
+export default InitialState;
