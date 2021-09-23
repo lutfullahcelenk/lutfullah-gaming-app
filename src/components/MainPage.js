6597011e05aa4228e5ab905181fd0cbbdd12a953
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-23 09:59:02 +0300

    taken value of context by destruction method

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,21 +2,30 @@ import React from "react";
 import Card from "./Card";
 import { useContext } from "react";
 import { gameContext } from "../context/gameContext";
+import { useHistory } from "react-router";
+
 
 const MainPage = () => {
 
-  const currentPosts = useContext(gameContext);
-  // console.log(currentPosts)
+  const {currentPosts} = useContext(gameContext);
+  const history = useHistory();
+
+  const goAddNominee = () => {
+    history.push({pathname: "/addpage"})
+  }
 
   return (
     <div>
       <h1 className="text-center">
         <strong>Vote </strong>for <strong>the Best Tournament</strong>
       </h1>
+
       <div className="d-flex justify-content-evenly">
-        <button className="btn btn-primary btn-lg">
+
+        <button className="btn btn-primary btn-lg" onClick={goAddNominee}>
           <i className="fas fa-plus me-2"></i>Add Nominee
         </button>
+
         <div className="dropdown">
           <button
             className="btn btn-secondary btn-lg"
@@ -28,6 +37,7 @@ const MainPage = () => {
           >
             <i className="fas fa-sort me-2"></i>Sort By
           </button>
+
           <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
             <button className="dropdown-item btn h5" href="#">
               Most Points
@@ -36,6 +46,7 @@ const MainPage = () => {
               Less Points
             </button>
           </div>
+          
         </div>
       </div>
       <div className="main-container row w-75 m-auto">
