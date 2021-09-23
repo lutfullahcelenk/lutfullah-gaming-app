6eed17fdc7583b0dfaecca4f78595b5875b4b760
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-23 10:00:17 +0300

    goHome button is activated and the form is written by bootstrap

--- src/pages/AddPage.js
+++ src/pages/AddPage.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router";
 
 const AddPage = () => {
-    return (
-        <div>
-            added
-        </div>
-    )
-}
+  const history = useHistory();
 
-export default AddPage
+  const goHome = () => {
+    history.push({ pathname: "/" });
+  };
+
+  return (
+    <div className="addpage">
+      <form className="form">
+        <div class="form-group">
+          <label for="exampleInputTournament" htmlFor="exampleInputTournament">
+            Tournament Name:
+          </label>
+
+          <input
+            type="text"
+            class="form-control"
+            id="exampleInputTournament"
+            placeholder="Enter a Tournamenat Name"
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="exampleInputWinner" htmlFor="exampleInputWinner">
+            Winner Team:
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleInputWinner"
+            placeholder="Enter the Winner Team"
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="exampleInputImage" htmlFor="exampleInputImage">
+            Cover Image URL:
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleInputImage"
+            placeholder="Enter Image URL"
+          />
+        </div>
+
+        <div className="d-flex justify-content-center mt-3">
+          <button type="submit" class="btn btn-warning">
+            Add Nominee
+          </button>
+        </div>
+      </form>
+
+      <button type="submit" class="btn btn-primary mt-5" onClick={goHome}>
+        Go back to Home
+      </button>
+    </div>
+  );
+};
+
+export default AddPage;
