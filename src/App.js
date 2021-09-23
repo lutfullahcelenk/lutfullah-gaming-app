c48db2c89981c8d7dd431b5491b1b50d3d2b1440
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-23 13:56:02 +0300

    BREAK-TIME --- localstorage is established. upPoint and downPoint fuctions are created.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import InitialState from "./store/InitialState";
 import { gameContext } from "./context/gameContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import AddPage from "./pages/AddPage";
-
-
 
 const App = () => {
   const [games, setGames] = useState(InitialState);
@@ -16,34 +14,69 @@ const App = () => {
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = games.slice(indexOfFirstPost, indexOfLastPost);
-  // console.log(currentPosts)
 
   // Change Page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   // Add Game
   const addNomineeGame = (game) => {
-    const id = Date.now()
-    const newGame = {...game,id}
-    setGames((prevState) => [...prevState, newGame])
+    const id = Date.now();
+    const newGame = { ...game, id };
+    setGames((prevState) => [...prevState, newGame]);
   };
-  
+
   // Delete Game
-  const deleteGame = (id,gname) => {
+  const deleteGame = (id, gname) => {
     let result = window.confirm(`Are you want to delete ${gname}?`);
-    if(result) {
-      setGames(games.filter((game)=> (
-        id !== game.id
-        )))
-    }else{
+    if (result) {
+      setGames(games.filter((game) => id !== game.id));
+    } else {
       return;
-    }  
+    }
   };
+
+  //Increase Game Point
+  const upPoint = (id, point) => {
+    point++;
+    setGames(
+      games.map((game) => game.id === id ? { ...game, point } : { ...game })
+    );
+  };
+
+  //Decrease Game Point
+  const downPoint = (id, point) => {
+    point--;
+    setGames(
+      games.map((game) => game.id === id ? { ...game, point } : { ...game })
+    );
+  };
+
+  //LocalStorage
+
+  useEffect(() => {
+    const data = localStorage.getItem("game-list");
+    if(data) {
+      setGames(JSON.parse(data))
+    }
+  },[]);
+
+  useEffect(() => {
+    localStorage.setItem("game-list", JSON.stringify(games))
+  });
 
   return (
     <Router>
       <gameContext.Provider
-        value={{ currentPosts, paginate, postsPerPage, games, addNomineeGame, deleteGame }}
+        value={{
+          currentPosts,
+          paginate,
+          postsPerPage,
+          games,
+          addNomineeGame,
+          deleteGame,
+          upPoint,
+          downPoint,
+        }}
       >
         <Switch>
           <Route exact path="/" component={Homepage} />
