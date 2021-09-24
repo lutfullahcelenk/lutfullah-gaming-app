2f4f69db9eca47406044c1b96bcdeb63e90125ad
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-24 14:14:28 +0300

    voteDate is added all list

--- src/store/InitialState.js
+++ src/store/InitialState.js
@@ -2,68 +2,61 @@
 const InitialState = [
   {
     id: 1,
-    point: 10,
-    name: "Cs Go",
-    winner: "Lütfullah",
-    image:
-      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+    point: 0,
+    name: "COUNTER STRIKE",
+    winner: "Alex",
+    lastVoteDate : "24.09.2021 09:19",
+    image : "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg"
   },
   {
     id: 2,
-    point: 10,
-    name: "Cs Go",
+    point: 0,
+    name: "LEAUGE OF LEGENDS",
     winner: "Lütfullah",
-    image:
-      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+    lastVoteDate : "15.06.2019 17:25",
+    image : "https://i2.milimaj.com/i/milliyet/75/0x410/5eff35145542810714a1971d.jpg"
   },
   {
     id: 3,
-    point: 10,
-    name: "Cs Go",
-    winner: "Lütfullah",
-    image:
-      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+    point: 0,
+    name: "CALL OF DUTY",
+    winner: "Mario",
+    lastVoteDate : "16.03.2021 04:19",
+    image : "https://images2.alphacoders.com/597/thumb-1920-597965.jpg"
   },
   {
     id: 4,
-    point: 10,
-    name: "Cs Go",
-    winner: "Lütfullah",
-    image:
-      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+    point: 0,
+    name: "PUBG",
+    winner: "Joseph",
+    lastVoteDate : "16.12.2021 04:29",
+    image : "https://upload.wikimedia.org/wikipedia/tr/thumb/c/c5/PlayerUnknown%27s_Battlegrounds_Steam_Ma%C4%9Faza_G%C3%B6rseli.jpg/220px-PlayerUnknown%27s_Battlegrounds_Steam_Ma%C4%9Faza_G%C3%B6rseli.jpg"
   },
   {
     id: 5,
-    point: 10,
-    name: "Cs Go",
-    winner: "Lütfullah",
-    image:
-      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+    point: 0,
+    name: "DOTA",
+    winner: "Michael",
+    lastVoteDate : "24.04.2021 09:19",
+    image : "https://i4.hurimg.com/i/hurriyet/75/750x422/5f28108067b0a921ecf469fa.jpg"
   },
   {
     id: 6,
-    point: 10,
-    name: "Cs Go",
-    winner: "Lütfullah",
-    image:
-      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+    point: 0,
+    name: "PES",
+    winner: "Catherine",
+    lastVoteDate : "12.03.2020 17:10",
+    image : "https://images.alphacoders.com/104/thumbbig-1046346.webp"
   },
   {
     id: 7,
-    point: 10,
-    name: "Cs Go",
-    winner: "Lütfullah",
-    image:
-      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
+    point: 0,
+    name: "NBA2K21",
+    winner: "Angel",
+    lastVoteDate : "15.09.2021 09:19",
+    image : "https://lh3.googleusercontent.com/LbXo8hN3dKfTnmfT-tFxm2cfafy9m96QARuxWOtnmxD-Pm_TuMKqm2WmF0ZHnNgJLFzfasI_d3ANTFxa9Co3FqUU=w640-h400-e365-rj-sc0x00ffffff"
   },
-  {
-    id: 8,
-    point: 10,
-    name: "Cs Go",
-    winner: "Lütfullah",
-    image:
-      "https://www.webtekno.com/images/editor/default/0003/10/ea1edc1663aea14ad280fe611937f48ff7e80210.jpeg",
-  },
+  
 ];
 
-export default InitialState;
+export default InitialState
